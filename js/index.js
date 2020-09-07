e579dbbf41a2209ca1b3021ac9e184f5de897614
Grgo Jelavic <9r9o@My-Mac.local>
2020-09-07 11:54:23 +0200

    Final changes CSS and JS

--- js/index.js
+++ js/index.js
@@ -2,11 +2,19 @@ let isSidebarOpen = false;
 
 function onSidebarToggle() {
     const sidebarRef = document.getElementById("sidebar-container");
+    const titleRef = document.getElementById("title-container")
+    const toggleRef = document.getElementById("toggle-container")
     isSidebarOpen = !isSidebarOpen;
 
     if (isSidebarOpen) {
         sidebarRef.classList.add("sidebar-open");
+        titleRef.classList.add("remove-title")
+        toggleRef.classList.add("toggle-background");
+
     } else {
         sidebarRef.classList.remove("sidebar-open");
+        titleRef.classList.remove("remove-title")
+        toggleRef.classList.remove("toggle-background");
+
     }
 }
